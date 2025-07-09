89e6b49b7d60eb58fde5e333c999b1e9d8299ff3
El Wafi Cherkaoui <elwafi.cherkaoui@gmail.com>
2025-07-09 23:17:58 +0100

    feat: add form validation using zod

--- src/app/register/registerForm.tsx
+++ src/app/register/registerForm.tsx
@@ -6,6 +6,20 @@ import { useEffect, useState } from "react";
 import { RegisterDataAction } from "../actions/actions";
 import { User } from "../../../generated/prisma";
 import { PhoneInput } from "@/components/ui/phone-input";
+import {z} from "zod"
+
+const RegisterSchema = z.object({
+  email: z.string().email({ message: 'Invalid email' }),
+  password: z.string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(20, { message: "Password must not exceed 20 characters" })
+    .regex(/[A-Z]/, { message: "Must contain at least one uppercase letter" })
+    .regex(/[a-z]/, { message: "Must contain at least one lowercase letter" })
+    .regex(/[0-9]/, { message: "Must contain at least one number" })
+    .regex(/[^a-zA-Z0-9]/, { message: "Must contain at least one symbol" }),
+  phoneNumber: z.string().length(13, { message: 'Invalid phone number' })
+})
+
 export default function RegisterForm() {
     const [formData, setFormData] = useState({email: "", password : "", c_password : "", phoneNumber: ""})
     const [phoneNumber, setPhoneNumber] = useState('')
@@ -24,6 +38,17 @@ export default function RegisterForm() {
         }
         const sentFormData = new FormData(e.currentTarget)
         sentFormData.set('phoneNumber', phoneNumber)
+
+        const rawData = Object.fromEntries(sentFormData.entries())
+        console.log(rawData);
+        
+        const result = RegisterSchema.safeParse(rawData)
+    
+        if (!result.success) {
+            throw new Error(
+                result.error.issues.map((i) => `${i.path[0]}: ${i.message}`).join("\n")
+            )
+        }
         RegisterDataAction(sentFormData)
         .then((res: User)=>{
             console.log(res)
